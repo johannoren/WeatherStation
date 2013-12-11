2772f750b1952145730a5808c90d7947e84cf3e0
Johan Noren <per.johan.noren@gmail.com>
2013-12-11 13:06:18 +0100

    Python source to read temp sensor and post readings to GAE REST interface for publishing on web site

---
+++ raspberrypi/src/weatherstation.py
@@ -0,0 +1,47 @@
+import os
+import glob
+import time
+import httplib
+import urllib
+
+os.system('sudo modprobe w1-gpio')
+os.system('sudo modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c
+
+while True:
+    try:
+        temp = read_temp()
+        print(temp)
+        params = urllib.urlencode({'temperature': temp})
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+        conn = httplib.HTTPConnection("192.168.1.4:8888")
+        conn.request("POST", "/api/temperature/stugan", params, headers)
+        response = conn.getresponse()
+        print response.status, response.reason
+        data = response.read()
+        conn.close()
+    except Exception, e:
+        print e.__doc__
+        print e.message
+    time.sleep(120)
