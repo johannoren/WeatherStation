39172c14d58825b897dd8fbfa3fb46a50903fc86
Johan Noren <per.johan.noren@gmail.com>
2013-12-21 22:05:05 +0100

    Fixes

--- src/se/noren/weatherstation/FormatUtil.java
+++ src/se/noren/weatherstation/FormatUtil.java
@@ -5,6 +5,10 @@ import java.math.BigDecimal;
 public class FormatUtil {
 
 	public static String round(double d, int decimals) {
-		return new BigDecimal(String.valueOf(d)).setScale(1, BigDecimal.ROUND_HALF_UP).toPlainString();
+		if (Double.isNaN(d)) {
+			return "N/A";
+		} else {
+			return new BigDecimal(String.valueOf(d)).setScale(1, BigDecimal.ROUND_HALF_UP).toPlainString();
+		}
 	}
 }
--- src/se/noren/weatherstation/WeatherStationServerServlet.java
+++ src/se/noren/weatherstation/WeatherStationServerServlet.java
@@ -1,9 +1,12 @@
 package se.noren.weatherstation;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
+import org.springframework.web.servlet.HttpServletBean;
+
 @SuppressWarnings("serial")
-public class WeatherStationServerServlet extends HttpServlet {
+public class WeatherStationServerServlet extends HttpServletBean {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
